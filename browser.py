9141d3856f2f166283f766a90872b16c8acaa72b
Josh Brodsky <90876419+TheJoshBrod@users.noreply.github.com>
2025-02-01 14:28:40 -0500

    browser options

--- browser.py
+++ browser.py
@@ -1,38 +1,48 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
-import os
-
 import winreg
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-def get_default_browser():
+
+
+def get_default_browser(browser=""):
     try:
         # Open the registry key that contains the default browser
         registry = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice")
         prog_id, _ = winreg.QueryValueEx(registry, "Progid")
         
+        if browser != "":
+            prog_id = browser 
         # Check if the prog_id contains Firefox
         if "firefox" in prog_id.lower():
-            return "Mozilla Firefox"
+            # Set up the Firefox driver
+            options = webdriver.FirefoxOptions()
+            return webdriver.Firefox(options=options)
+        elif "chrome" in prog_id.lower():
+            options = webdriver.ChromeOptions()
+            return webdriver.Chrome(options=options)
+        elif "edge" in prog_id.lower():
+            options = webdriver.EdgeOptions()
+            
+            # Setup the WebDriver with the EdgeDriverManager
+            driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()), options=options)
+  
+            return driver
         else:
             return "Unknown Browser"
     except Exception as e:
         return f"Error: {e}"
 
-print(f"Default Browser: {get_default_browser()}")
 
 
+def main() -> None:
+   
+    driver = get_default_browser("edge")
+    driver.get('https://google.com')
+    while True:
+        continue
 
 
-# Set up the Firefox driver
-options = webdriver.FirefoxOptions()
-driver = webdriver.Firefox(options=options)
-
-# Open a website
-driver.get('https://google.com')
-
-# Wait for the page to load (optional)
-time.sleep(3)
-
-# Close the tab
-driver.close()
+if __name__ == "__main__":
+    main() 
